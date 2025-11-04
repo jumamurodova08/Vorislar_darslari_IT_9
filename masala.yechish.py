6c71aa2053c907476f781c51fc25954eb2fa9001
jumamurodova08 <jumamurodovasharofat08@gmail.com>
2025-11-04 07:59:41 -0400

    first commit

---
+++ masala.yechish.py
@@ -0,0 +1,16 @@
+#1-masala. k va n butun sonlari berilgan (n>0).k sonini n marta chiqaruvchi programma tuzilsin.
+# print("n>0")
+# k = int(input("k sonini kiriting:"))
+# n = int(input("n sonini kiriting:"))
+
+# for son in range(1,n+1):
+#    print(f"{son}-sikl : {k}")
+
+#2-masala.
+a = int(input("a sonini kiriting:"))
+b = int(input("b sonini kiriting:"))
+sonlar = list(range(a,b+1))
+for son in sonlar:
+    print(son)
+print(len(sonlar))
+
